38e9e57b6387f2536526cda1024dce6f3466bbfa
Akash <akashorasad2000@gmail.com>
2020-02-25 23:45:11 +0530

    updating our application with reselect,replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
 
 // import CartIcon from "../cart-icon/cart-icon.component";
 
-const CartDropdown = ({ cartItems }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+const CartDropdown = ({ cartItems }) => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
-  </div>
-);
+  );
+};
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//   cartItems
+// });
+
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps, null)(CartDropdown);
